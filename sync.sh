482f4aa7c07a66c19a3e99a37f87adc6b595ee51
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2012-02-27 09:47:40 +0900

    Add COMMENT parameter

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ wget $ICAL_URI -O private.ics
 # Simplify SUMMARY and drop DESCRIPTION
 sed 's/^SUMMARY:\(.\).*/SUMMARY:\1/g' private.ics | grep -v ^DESCRIPTION > public.ics
 git add public.ics
-git commit -m "test"
+git commit -m "$COMMENT"
 # Uncomment this
 # git push $REMOTE_GIT gh-pages
 
