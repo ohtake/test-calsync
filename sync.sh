15fbd22a37ee20a61e2b7a5de9316330932818ee
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2012-02-28 08:48:36 +0900

    Delete SUMMARY even if SUMMARY starts with non-UCS-2 char

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ fi
 git checkout gh-pages
 wget $ICAL_URI -O private.ics
 # Simplify SUMMARY (single UCS-2 char in UTF-8) and drop DESCRIPTION
-perl -pe 's/^SUMMARY:([\x00-\x7F]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEF][\x80-\xBF][\x80-\xBF]).*/SUMMARY:\1/g' < private.ics | grep -v ^DESCRIPTION > public.ics
+perl -pe 's/^SUMMARY:([\x00-\x7F]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEF][\x80-\xBF][\x80-\xBF])?.*/SUMMARY:\1/g' < private.ics | grep -v ^DESCRIPTION > public.ics
 git add public.ics
 git commit -m "$COMMENT"
 # Uncomment this
