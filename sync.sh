95f69b7a48e6d30376a7a62c7f37cadc44c336e5
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2012-02-27 09:18:37 +0900

    gh-pages

--- sync.sh
+++ sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 . config.sh
-git checkout work
+git checkout gh-pages
 wget $ICAL_URI -O private.ics
 # Simplify SUMMARY and drop DESCRIPTION
 sed 's/^SUMMARY:\(.\).*/SUMMARY:\1/g' private.ics | grep -v ^DESCRIPTION > public.ics
 git add public.ics
 git commit -m "test"
 # Uncomment this
-# git push $REMOTE_GIT work
+# git push $REMOTE_GIT gh-pages
 
