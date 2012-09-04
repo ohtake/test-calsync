357ba41d982e74f8c3601dfda1b9e946b7d0a239
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2012-09-04 14:12:45 +0900

    Export ical from Outlook

---
+++ outlook2ical.ps1
@@ -0,0 +1,46 @@
+﻿$private = (Join-Path (pwd) "private.ics")
+$public = (Join-Path (pwd) "public.ics")
+
+$outlook = New-Object -ComObject Outlook.Application
+$folder = $outlook.Session.GetDefaultFolder(9) # olFolderCalendar = 9
+$exporter = $folder.GetCalendarExporter()
+$exporter.CalendarDetail = 2 # olFullDetails = 2
+$exporter.EndDate = [DateTime]::Now.Date.AddMonths(2)
+$exporter.SaveAsICal($private)
+
+function normalize-ical([string[]]$lines) {
+    $lines |% -Begin {
+        $temp = ""
+    } -Process {
+        if($_ -match "^\t(.*)") {
+            $temp += $Matches[1]
+        } else {
+            $ret = $temp
+            $temp = $_
+            if ($ret -ne "") {$ret}
+        }
+    } -End {
+        if($temp -ne "") {
+            $temp
+        }
+    }
+}
+function replace-summary([string]$str) {
+    if ($str -match "^SUMMARY:\[TW\] (.)?.*") {
+        return "SUMMARY:[TW] " + $Matches[1]
+    } elseif ($str -match "^SUMMARY(?:;[^;:]*)*:(.)?.*") {
+        return "SUMMARY:" + $Matches[1]
+    } else {
+        return $str
+    }
+}
+function is-description([string]$str) {
+    $str -match "^(?:DESCRIPTION:|X-ALT-DESC[;:])"
+}
+
+$lines = cat $private -Encoding UTF8
+$lines = normalize-ical $lines
+$lines = $lines |% {replace-summary($_)}
+$lines = $lines |? {(is-description($_)) -eq $false}
+$lines | Out-File -Encoding UTF8 $public
+
--- sync.sh
+++ sync.sh
@@ -3,9 +3,7 @@ if [ -f config.sh ]; then
   . config.sh
 fi
 git checkout gh-pages
-wget "$ICAL_URI" -O private.ics
-# Simplify SUMMARY (single UCS-2 char in UTF-8) and drop DESCRIPTION
-perl -pe 's/^SUMMARY:([\x00-\x7F]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEF][\x80-\xBF][\x80-\xBF])?.*/SUMMARY:\1/g' < private.ics | grep -v ^DESCRIPTION > public.ics
+powershell.exe outlook2ical.ps1
 git add -N public.ics
 git add public.ics `[ "$INTERACTIVE" = "true" ] && echo "-p"`
 git commit -m "$COMMENT"
